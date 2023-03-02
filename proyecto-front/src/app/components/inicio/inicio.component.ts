70a57fe766390657f5fdad49c3cea4825d8c7604
Thiagosgcnoah <117236033+Thiagosgcnoah@users.noreply.github.com>
2023-03-01 20:58:52 -0500

    Verifi

--- proyecto-front/src/app/components/inicio/inicio.component.ts
+++ proyecto-front/src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+
+  contactoForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+    this.contactoForm = this.fb.group({
+
+        nombre:['', Validators.required],
+        correo:['', Validators.required, Validators.email],
+        numero:['', Validators.required],
+        mensaje:['', Validators.required]
+    })
+
+  }
+  ngOnInit(): void {
+
+  }
+  crearContacto(){
+    console.log(this.contactoForm);
+    console.log(this.contactoForm.get('nombre'));
+  }
 
 }
