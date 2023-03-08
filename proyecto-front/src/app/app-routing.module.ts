e140c043248bfcf84c868ac1fb1b07ed610c2353
SebastianRodriguez02 <117236083+SebastianRodriguez02@users.noreply.github.com>
2023-03-08 15:54:00 -0500

    admi (#9)
    git commit -m admii
    git commit -m admi
    
    Co-authored-by: Linaburgos7 <117236093+Linaburgos7@users.noreply.github.com>

--- proyecto-front/src/app/app-routing.module.ts
+++ proyecto-front/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdmiUsersComponent } from './components/admi/admi-users/admi-users.component';
+import { GestionContactoComponent } from './components/admi/gestion-contacto/gestion-contacto.component';
 import { GestionProductosComponent } from './components/admi/gestion-productos/gestion-productos.component';
+import { ListaContactosComponent } from './components/admi/lista-contactos/lista-contactos.component';
 import { ListaProductosComponent } from './components/admi/lista-productos/lista-productos.component';
 import { GatosComponent } from './components/gatos/gatos.component';
 import { InicioComponent } from './components/inicio/inicio.component';
@@ -16,6 +18,9 @@ const routes: Routes = [
   {path: 'editarproducto/:id', component:GestionProductosComponent},
   {path:"administrador",component:AdmiUsersComponent},
   {path:"listaproductos",component:ListaProductosComponent},
+  {path:"gestioncontactos",component:GestionContactoComponent},
+  {path:"editarcontacto/:id",component:GestionContactoComponent},
+  {path:"listacontactos",component:ListaContactosComponent},
 
 
 ];
--- proyecto-front/src/app/app.module.ts
+++ proyecto-front/src/app/app.module.ts
@@ -14,7 +14,10 @@ import { GatosComponent } from './components/gatos/gatos.component';
 import { AdmiUsersComponent} from './components/admi/admi-users/admi-users.component';
 import { GestionProductosComponent } from './components/admi/gestion-productos/gestion-productos.component';
 import { ListaProductosComponent } from './components/admi/lista-productos/lista-productos.component';
+import { GestionContactoComponent } from './components/admi/gestion-contacto/gestion-contacto.component';
+import { ListaContactosComponent } from './components/admi/lista-contactos/lista-contactos.component';
 import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +28,9 @@ import { FormsModule } from '@angular/forms';
     GatosComponent,
     AdmiUsersComponent,
     GestionProductosComponent,
-    ListaProductosComponent
+    ListaProductosComponent,
+    GestionContactoComponent,
+    ListaContactosComponent
   ],
   imports: [
     BrowserModule,
